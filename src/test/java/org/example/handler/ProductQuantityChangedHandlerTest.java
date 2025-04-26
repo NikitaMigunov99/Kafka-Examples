84bbf11c92c96eb195b1406b3128ec9453ac6153
Nikita <nikita1999.nm@gmail.com>
2025-04-26 19:48:02 +0300

    fix beans

--- src/test/java/org/example/handler/ProductQuantityChangedHandlerTest.java
+++ src/test/java/org/example/handler/ProductQuantityChangedHandlerTest.java
@@ -5,6 +5,8 @@ import org.example.models.event.ProductQuantityChangedEvent;
 import org.example.utils.BaseTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.test.utils.KafkaTestUtils;
 
 import java.time.Duration;
@@ -15,6 +17,9 @@ public class ProductQuantityChangedHandlerTest extends BaseTest {
 
     @Test
     public void getEvent() {
+        var factory = new DefaultKafkaProducerFactory<>(getProducerProperties());
+        var kafkaTemplate = new KafkaTemplate<>(factory);
+
         ProductQuantityChangedEvent event = new ProductQuantityChangedEvent("Some ID", 5);
         kafkaTemplate.send("product-quantity-changed-events-topic", event);
 
--- src/test/java/org/example/utils/BaseTest.java
+++ src/test/java/org/example/utils/BaseTest.java
@@ -1,15 +1,9 @@
 package org.example.utils;
 
-import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 import org.springframework.test.context.DynamicPropertyRegistry;
@@ -17,7 +11,6 @@ import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.utility.DockerImageName;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @SpringBootTest
@@ -37,25 +30,6 @@ public abstract class BaseTest {
         registry.add("spring.kafka.bootstrap-servers", kafkaContainer::getBootstrapServers);
     }
 
-    @Autowired
-    protected KafkaTemplate<String, Object> kafkaTemplate;
-
-    @Bean
-    public ProducerFactory<String, Object> producerFactory() {
-        Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
-        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-        configProps.put(ProducerConfig.ACKS_CONFIG, "all");
-        configProps.put(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, 1000);
-        return new DefaultKafkaProducerFactory<>(configProps);
-    }
-
-    @Bean
-    public KafkaTemplate<String, Object> kafkaTemplate(ProducerFactory<String, Object> factory) {
-        return new KafkaTemplate<>(factory);
-    }
-
     protected Map<String, Object> getConsumerProperties() {
         return Map.of(
                 "bootstrap.servers", bootstrapAddress,
@@ -64,5 +38,14 @@ public abstract class BaseTest {
                 "key.deserializer", StringDeserializer.class,
                 "value.deserializer", JsonDeserializer.class
         );
+    }
+
+    protected Map<String, Object> getProducerProperties() {
+        return Map.of(
+                "bootstrap.servers", bootstrapAddress,
+                "acks", "all",
+                "delivery.timeout.ms", 1000,
+                "key.serializer", StringSerializer.class,
+                "value.serializer", JsonSerializer.class);
     }
 }
