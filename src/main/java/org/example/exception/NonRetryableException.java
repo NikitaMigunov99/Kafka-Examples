f0d0853bf6f465abe15f6685b8f0a4a49064900a
Nikita <nikita1999.nm@gmail.com>
2025-04-28 15:31:31 +0300

    add exceptions

---
+++ src/main/java/org/example/exception/NonRetryableException.java
@@ -0,0 +1,16 @@
+package org.example.exception;
+
+public class NonRetryableException extends RuntimeException {
+
+    public NonRetryableException(String message) {
+        super(message);
+    }
+
+    public NonRetryableException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NonRetryableException(Throwable cause) {
+        super(cause);
+    }
+}
